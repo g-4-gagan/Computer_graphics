8b1bfcdc6b0acf4145d2577f445c3be0872af3d6
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-13 00:01:48 +0530

    bresenham for line drawing

---
+++ bresenhams_line.cpp
@@ -0,0 +1,41 @@
+//1. Write a program to implement Bresenhams line drawing
+algorithm.
+#include<stdio.h>
+#include<graphics.h>
+void midpointline(int x0,int y0,int x1,int y1,int value)
+{
+double dy=y1-y0;
+double dx=x1-x0;
+int d=2*dy-dx;
+int incrE=2*dy;
+int increNE=2*(dy-dx);
+int x=x0;
+int y=y0;
+putpixel(x,y,value);
+while(x<x1)
+{ //choose E
+if(d<0)
+{
+d+=incrE;
+x++;
+}
+else
+{
+d+=increNE;
+x++;
+y++;
+}
+putpixel(x,y,value);
+}
+}
+main()
+P a g e | 3
+{
+int gd=DETECT,gm;
+int i,j,k;
+initgraph(&gd,&gm,"c:\\turboc3\\bgi");
+midpointline(100,100,200,200,3);
+getch();
+closegraph();
+return 0;
+}
